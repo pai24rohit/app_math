1324bb78ef625191b33240ef7b6974f8420bb67f
Dinkarpai <dinkarnandkumar.pai@georgebrown.ca>
2024-05-17 12:37:57 -0400

    Modified_version

---
+++ Dinkar_Pai.py
@@ -0,0 +1,2 @@
+def hi_my_name_is():
+    return "Dinkar Nandkumar Pai" # Write your name inside the quotation marks
---
+++ ex1_test.py
@@ -0,0 +1,5 @@
+import Dinkar_Pai as name_point
+
+def test_hi_my_name_is():
+    assert len(name_point.hi_my_name_is())  > 1 
+
